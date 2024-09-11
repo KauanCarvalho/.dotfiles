bb0a4d4596d92ee29c9d7621b86fb489f3fba657
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2024-09-11 17:33:16 -0300

    feat: adds basic structure to zshrc

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -eu pipefail
 
 install_dotfiles () {
-  dotfiles=( aliases asdfrc default-gems gemrc gitattributes )
+  dotfiles=( aliases asdfrc default-gems gemrc gitattributes zshrc )
 
   for dotfile in "${dotfiles[@]}";
   do
