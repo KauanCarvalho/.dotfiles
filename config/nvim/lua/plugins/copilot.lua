3830b6435c864dd2318be54100297d9cacc38018
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2024-09-13 00:06:15 -0300

    refactor: adjusts copilot plugin identation

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,14 +1,17 @@
 return {
-	"github/copilot.vim",
-	config = function()
-		vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-		vim.g.copilot_no_tab_map = true
+  "github/copilot.vim",
+  config = function()
+    vim.api.nvim_set_keymap("i", "<C-h>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.g.copilot_no_tab_map = true
 
-		vim.g.copilot_filetypes = {
-			["go"] = true,
-			["yaml"] = true,
-			["markdown"] = false,
-			[""] = true,
-		}
-	end,
+    vim.g.copilot_filetypes = {
+      ["elixir"] = true,
+      ["go"] = true,
+      ["javascript"] = true,
+      ["markdown"] = false,
+      ["ruby"] = true,
+      ["yaml"] = true,
+      [""] = true,
+    }
+  end,
 }
