7495a5604e4c059ecd9578563b20d0fa33806016
Kauan Carvalho Calasans <kauan.carvalho.calasans@gmail.com>
2024-09-24 17:29:42 -0300

    feat: changes colorscheme for nvim

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,46 +1,75 @@
 return {
-  {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
-    config = function()
-      require('kanagawa').setup({
-        compile = false,
-        undercurl = true,
-        commentStyle = {
-          italic = true
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    require("catppuccin").setup({
+      flavour = "macchiato",
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      show_end_of_buffer = false,
+      term_colors = true,
+      dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      default_integrations = true,
+      integrations = {
+        cmp = true,
+        nvimtree = true,
+        treesitter = true,
+        telescope = {
+          enabled = true
         },
-        functionStyle = {},
-        keywordStyle = {
-          italic = true
-        },
-        statementStyle = {
-          bold = true
-        },
-        typeStyle = {},
-        transparent = false,
-        dimInactive = false,
-        terminalColors = true,
-        colors = {
-          palette = {},
-          theme = {
-            wave = {},
-            lotus = {},
-            dragon = {},
-            all = {}
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+          inlay_hints = {
+            background = true,
           },
         },
-        overrides = function(colors)
-          return {}
-        end,
-        theme = "wave",
-        background = {
-          dark = "dragon",
-          light = "lotus"
+        indent_blankline = {
+          enabled = true,
+          scope_color = "",
+          colored_indent_levels = false,
         },
-      })
+      }
+    })
 
-      vim.cmd.colorscheme("kanagawa-dragon")
-    end
-  }
+    vim.cmd.colorscheme("catppuccin-macchiato")
+  end
 }
